16856e8dcd7e34bdab9168b87bdf37e37b3eced0
Abhishek Garg <agarg5@alumni.stanford.edu>
2025-12-03 20:29:55 +0100

    use wisper model directly

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 load_dotenv(PROJECT_ROOT / ".env")
 
 HOUSE_URL = "https://house.mi.gov/VideoArchive"
-SENATE_URL = "https://cloud.castus.tv/vod/misenate/?page=ALL"
+SENATE_URL = "https://2kbyogxrg4.execute-api.us-west-2.amazonaws.com/61b3adc8124d7d000891ca5c/home/recent"
 
 TRANSCRIPTION_MODEL = os.getenv("TRANSCRIPTION_MODEL", "gpt-4o-transcribe")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
