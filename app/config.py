98d60fdbc4d4c5bfb4d5aa1a9f6ceae534441404
Abhishek Garg <agarg5@alumni.stanford.edu>
2025-12-04 18:18:50 -0500

    add build tools

--- app/config.py
+++ app/config.py
@@ -10,7 +10,6 @@ HOUSE_URL = "https://house.mi.gov/VideoArchive"
 SENATE_URL = "https://2kbyogxrg4.execute-api.us-west-2.amazonaws.com/61b3adc8124d7d000891ca5c/home/recent"
 
 TRANSCRIPTION_MODEL = os.getenv("TRANSCRIPTION_MODEL", "gpt-4o-transcribe")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
 DAYS_BACK = int(os.getenv("DAYS_BACK", "60"))
 DATA_DIR = "data"
